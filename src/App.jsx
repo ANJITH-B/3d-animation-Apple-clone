29c515edcbeff811a1ed302bee759b4d8ed960e1
Abhijith Babu <abijithb2002@gmail.com>
2023-11-21 11:07:58 +0530

    soundSection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Jumbotron from "./compenents/Jumbotron";
 import Nav from "./compenents/Nav";
+import SoundSection from "./compenents/SoundSection";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Nav/>
       <Jumbotron/>
+      <SoundSection/>
     </div>
   );
 }
--- src/compenents/Jumbotron.jsx
+++ src/compenents/Jumbotron.jsx
@@ -3,6 +3,16 @@ import Iphone from "../assets/images/iphone-14.jpg";
 import HoldingIphone from "../assets/images/iphone-hand.png";
 
 function Jumbotron() {
+
+    const handleLearnMore = () => {
+        const element = document.querySelector('.sound-section');
+        window.scrollTo({
+            top: element?.getBoundingClientRect().top,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
     return (<div className="jumbotron-section wrapper">
         <h2 className="title">New</h2>
         <imag className="logo" src={Iphone} alt='iphone 14 pro' />
@@ -12,7 +22,7 @@ function Jumbotron() {
         </span>
         <ul className="links">
             <li><button className="button">Buy</button></li>
-            <li><a className="link">LearnMore</a></li>
+            <li><a className="link" onClick={handleLearnMore}>LearnMore</a></li>
         </ul>
         <img className="iphone-img" src={HoldingIphone} alt="iphone"/>
 
